a9b94adedc69f86336f21556ce82fc8dfe78453d
Kevin Tate <2kmtate@gmail.com>
2018-12-05 16:46:49 -0500

    Added score/timer

--- resources/js/match-game.js
+++ resources/js/match-game.js
@@ -1,13 +1,15 @@
 var MatchGame = {};
 var checkForWin = 0;
 const $game = $('#game');
+let timer;
 
-/*
-  Sets up a new game after HTML document has loaded.
-  Renders a 4x4 board of cards.
-*/
 $(document).ready(function() {
 	MatchGame.renderCards(MatchGame.generateCardValues(), $game);
+	$('.timer').data('timer', 0);
+
+	timer = setInterval(function() {
+		MatchGame.incrementTimer($('.timer'));
+	}, 1000);
 
 	$('.card').click(function() {
 		if ($game.data('flippedCards').length !== 2) {
@@ -15,10 +17,6 @@ $(document).ready(function() {
 		};
 	});
 });
-
-/*
-  Generates and returns an array of matching card values.
- */
 
 MatchGame.generateCardValues = function () {
 	var initialArray = [];
@@ -34,38 +32,6 @@ MatchGame.generateCardValues = function () {
 	}
 	return randomArray;
 };
-
-/*
-  Converts card values to jQuery card objects and adds them to the supplied game
-  object.
-*/
-
-MatchGame.renderCards = function(cardValues, $game) {
-	$game.data('flippedCards', []);
-	var flippedColors = [
-		'hsl(25, 85%, 65%)', 
-		'hsl(55, 85%, 65%)', 
-		'hsl(90, 85%, 65%)',
-		'hsl(160, 85%, 65%',
-		'hsl(220, 85%, 65%',
-		'hsl(265, 85%, 65%',
-		'hsl(310, 85%, 65%',
-		'hsl(360, 85%, 65%'
-	];
-	$game.empty();
-	for (var i = 0; i < cardValues.length; i++) {
-		var $card = $('<div class="col-lg-3 card"></div>');
-		$card.data('value', cardValues[i]);
-		$card.data('flipped', false);
-		$card.data('color', flippedColors[cardValues[i]-1]);
-		$game.append($card);
-	}
-};
-
-/*
-  Flips over a given card and checks to see if two cards are flipped over.
-  Updates styles on flipped cards depending whether they are a match or not.
- */
 
 MatchGame.flipCard = function($card, $game) {
 	if ($card.data('flipped')) {
@@ -102,11 +68,41 @@ MatchGame.flipCard = function($card, $game) {
 
 MatchGame.gameOver = function() {
 	const $victory = $('.victory');
+	clearInterval(timer);
+	$('.score').text($('.timer').data('timer') + 's');
 
 	$('.victory-overlay').css('display', 'block');
 	$('.victory-wrapper').css('z-index', '1000');
 	$victory.css('display', 'block');
 	$victory.click(function() {
 		location.reload();
-	})
-}
+	});
+};
+
+MatchGame.renderCards = function(cardValues, $game) {
+	$game.data('flippedCards', []);
+	var flippedColors = [
+		'hsl(25, 85%, 65%)', 
+		'hsl(55, 85%, 65%)', 
+		'hsl(90, 85%, 65%)',
+		'hsl(160, 85%, 65%',
+		'hsl(220, 85%, 65%',
+		'hsl(265, 85%, 65%',
+		'hsl(310, 85%, 65%',
+		'hsl(360, 85%, 65%'
+	];
+	$game.empty();
+	for (var i = 0; i < cardValues.length; i++) {
+		var $card = $('<div class="col-lg-3 card"></div>');
+		$card.data('value', cardValues[i]);
+		$card.data('flipped', false);
+		$card.data('color', flippedColors[cardValues[i]-1]);
+		$game.append($card);
+	}
+};
+
+MatchGame.incrementTimer = function($timer) {
+	let currentTime = $timer.data('timer') + 1;
+	$timer.text(currentTime + 's');
+	$timer.data('timer', currentTime);
+};
